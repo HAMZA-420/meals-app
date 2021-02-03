bcb15bb8a0bbf2e4fe813866776c803d41966558
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-03 10:14:38 +0500

    Adding Screens and Fonts

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import {Apploading } from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans' : require('./assets/fonts/OpenSanas-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontloaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <Apploading
+      startAsync = {fetchFonts}
+      onFinish={() => setFontloaded(true)}
+      onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
