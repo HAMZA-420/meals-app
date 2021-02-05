2c595629433ef3b9411aaf3503440e398db6e56c
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-05 10:59:09 +0500

    Grid+ Header with Navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Platform,Text, StyleSheet, Button,FlatList, TouchableOpacity} from 'react-native';
+import {CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
 
 const CategoriesScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text>The CategoriesScreen</Text>
-            <Button title="Go to Meals" onPress={() => {
-                props.navigation.navigate('CategoryMeals');
-            }
-            } />
+
+    const renderGridItem = (itemData) => {
+        return (
+        <TouchableOpacity 
+        style={styles.gridItem}
+        onPress={() => {
+            props.navigation.navigate({routeName: 'CategoryMeals'});
+        }}>
+        <View>
+            <Text>{itemData.item.title}</Text>
         </View>
+        </TouchableOpacity>
+        );
+    };
+
+    return (
+        
+       <FlatList 
+       keyExtractor={(item, index) => item.id}
+       data={CATEGORIES} 
+       renderItem={renderGridItem}
+       numColumns={2}/>
     );
+};
+
+CategoriesScreen.navigationOptions ={
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android'? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS ==='android' ? 'white' : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -18,6 +43,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -9,6 +9,9 @@ const CategoryMealsScreen = props => {
                 props.navigation.navigate('MealDetail');
             }
             } />
+            <Button title="Go Back" onPress={() => {
+                props.navigation.pop();
+            }} />
         </View>
     );
 };
