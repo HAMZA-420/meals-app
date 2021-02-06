f3f3671278e01231fd19753be7a4ed16d80687f7
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-06 08:04:33 +0500

    fixing shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -26,16 +26,16 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS ==='android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+        elevation: 5,
     },
     container: {
         flex: 1,
-        borderRadius: 10,
         shadowColor: 'black',
         shadowOpacity: 0.26,
         shadowOffset: {width: 0, height: 2},
         shadowRadius: 10,
-        elevation: 3,
+        borderRadius: 10, 
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
