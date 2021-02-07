df4919d02c79c9c6d951e30cb6a607dc9b5b0319
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-07 09:07:19 +0500

    Adding material bottom tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -5,10 +5,11 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
-import {Platform} from 'react-native';
+import {ColorPropType, Platform} from 'react-native';
 import Colors from '../constants/Colors';
 import FavouriteScreen  from '../screens/FavouriteScreen'; 
 import {Ionicons} from '@expo/vector-icons';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 
 const MealsNavigator= createStackNavigator({
     Categories: CategoriesScreen,
@@ -28,18 +29,33 @@ const MealsNavigator= createStackNavigator({
 }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+const tabScreenConfig = {
     Meals: {screen: MealsNavigator, navigationOptions: {
         tabBarIcon: (tabInfo) => {
             return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
-        }
+        },
+        tabBarColor: Colors.primaryColor
     }},
     Favorites: {screen: FavouriteScreen, navigationOptions: {
         tabBarIcon: (tabInfo) => {
-            return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
-        }
+            return (
+            <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+            );
+        },
+        tabBarColor: Colors.accentColor
     }}
-},  {
+};
+
+const MealsFavTabNavigator =
+ Platform.OS==='android' ? createMaterialBottomTabNavigator(tabScreenConfig, {
+     activeColor: 'white',
+     shifting: true,
+     barStyle: {
+         backgroundColor: Colors.primaryColor
+     }
+ }) :createBottomTabNavigator(
+    tabScreenConfig
+    ,  {
     tabBarOptions: {
         activeTintColor: Colors.accentColor
     }
