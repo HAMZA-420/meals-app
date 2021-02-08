41c4b6614ae65dcac767167c0045587d6d8e720d
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-08 14:09:34 +0500

    Adding DefaultText

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   ImageBackground
 } from 'react-native';
+import DefaultText from './DefaultText';
 
 const MealItem = props => {
   return (
@@ -25,9 +26,9 @@ const MealItem = props => {
             </ImageBackground>
           </View>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <Text>{props.duration}m</Text>
-            <Text>{props.complexity.toUpperCase()}</Text>
-            <Text>{props.affordability.toUpperCase()}</Text>
+            <DefaultText>{props.duration}m</DefaultText>
+            <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+            <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
           </View>
         </View>
       </TouchableOpacity>
